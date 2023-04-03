c8ca933ea5e914878e0524290f234588d96d858d
romulogdonadoni <romulogdonadoni@gmail.com>
2023-04-03 01:48:42 -0300

    app-estudo-back-end 0.5

--- src/server.js
+++ src/server.js
@@ -2,39 +2,55 @@ import express from "express";
 import mongoose from "mongoose";
 import { Users } from "./models/users.js";
 import jwt from "jsonwebtoken";
-
+const SECRET = "5lBRWNHZJDIgeL1gtBLFzalN4DXl79DO";
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 mongoose
   .connect("mongodb+srv://romegd:s88Oc4G36gMIOZyQ@dbestuds.wojdxiz.mongodb.net/?retryWrites=true&w=majority")
   .then(() => {
-    app.listen(3333)
+    app.listen(3333);
     console.log("Connected!");
   })
   .catch((error) => console.log(error));
+
+function authHomePage(req, res, next) {
+  const token = req.headers["Authorization"];
+  jwt
+    .verify(token)
+    .then(() => {
+      console.log('ok')
+      next();
+    })
+    .catch(() => {
+      return res.status(401).end();
+    });
+}
+app.get("/homepage", authHomePage, (req, res) => {
+  return res.status(200).json({msg: "Autorizado"}).end()
+});
 
 app.post("/auth/login", async (req, res) => {
   const { username, password } = req.body;
   if (!req.body.username || !req.body.password) {
     return res.status(400).end("Insira os dados!");
   }
-  const userExists = await Users.findOne({ username: username });
+  const user = await Users.findOne({ username: username });
 
-  if (!userExists) {
-    return res.status(404).json({msg:"Usuário não encontrado"}).end();
+  if (!user) {
+    return res.status(404).json({ msg: "Usuário não encontrado" }).end();
   }
-  const checkpassword = await password === userExists.password;
+  const checkpassword = (await password) === user.password;
   if (!checkpassword) {
-    return res.status(400).json({msg:"Senha inválida!"}).end();
+    return res.status(400).json({ msg: "Senha inválida!" }).end();
   }
 
   try {
     const token = jwt.sign(
       {
-        id: userExists._id,
+        id: user._id,
       },
-      "5lBRWNHZJDIgeL1gtBLFzalN4DXl79DO"
+      SECRET
     );
     return res.status(200).json({ msg: "Usuário logado com sucesso!", token }).end();
   } catch (error) {
@@ -60,4 +76,3 @@ app.post("/auth/register", async (req, res) => {
   }
   return res.status(500).end();
 });
-
